24cfd83f997f21188fbdc65870ec9f4c8e6c6b0a
PedroDev03 <pedroaugustor03@gmail.com>
2025-04-27 23:23:34 -0300

    versao inicial

---
+++ src/main/java/br/progweb/ProgwebCrud/ProgwebCrudApplication.java
@@ -0,0 +1,13 @@
+package br.progweb.ProgwebCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgwebCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgwebCrudApplication.class, args);
+	}
+
+}
